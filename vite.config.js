ea5a2c4b8a1e5dfdd7900965a7969af32b27cb56
satan-696 <rghvshrma135@gmail.com>
2026-03-23 17:29:20 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,16 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          'framer-motion': ['framer-motion'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'vendor';
+            }
+            if (id.includes('framer-motion')) {
+              return 'framer-motion';
+            }
+            return 'vendor';
+          }
         },
       },
     },
